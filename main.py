9ee37a071a3f8138c75d9a94175cbaebfae3573f
Dursun Gokoglan <dursungokoglan.professional@gmail.com>
2023-12-09 16:06:56 +0300

    fix bug

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import smtplib
 import datetime as dt
 import pandas as pd
 from random import randint
+import credentials
 
 file = pd.read_csv("birthdays.csv")
 df = pd.DataFrame(file)
 
 name = df.name.iloc[0]
 email = df.email.iloc[0]
-year = df.year.iloc[0]
 month = df.month.iloc[0]
 day = df.day.iloc[0]
-birthday = (year, month, day)
+birthday = (month, day)
 now = dt.datetime.now()
 
-if birthday == (now.year, now.month, now.day):
+if birthday == (now.month, now.day):
     letter_no = randint(1, 3)
     with open(f"letter_templates/letter_{letter_no}.txt", "r") as letter:
         contents = letter.read()
         contents = contents.replace("[NAME]", name)
 
-    my_email = "[*YOUR EMAIL]"
-    password = "[*YOUR PASSWORD]"
+    my_email = credentials.my_email
+    password = credentials.password
 
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
@@ -31,4 +31,3 @@ if birthday == (now.year, now.month, now.day):
             to_addrs=email,
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
-        
